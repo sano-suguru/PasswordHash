56501052c53d1e1b5be34e165aaa6859463667ba
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-11 00:03:35 +0900

    [add]Userモデルを作成

---
+++ PasswordHash.App/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordHash.App.Models {
+  public class User {
+    [Key] public string Name { get; set; }
+    public string SaltString { get; set; }
+    public string HashedPassword { get; set; }
+  }
+}
