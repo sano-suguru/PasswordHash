0fc0bae3cf0d4b5065c45b110b8d5e3bf9501841
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-10 23:12:09 +0900

    [add]IPasswordService.cs

---
+++ PasswordHash.Lib/IPasswordService.cs
@@ -0,0 +1,6 @@
+﻿namespace PasswordHash.Lib {
+  public interface IPasswordService {
+    bool CheckPassword(string rawPassword, string hashedPassword, string saltSring);
+    (string hashedPassword, string salt) HashPassword(string rawPassword);
+  }
+}
