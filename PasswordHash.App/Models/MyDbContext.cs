2453110caba73773c334471387946cb7883f249f
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-11 00:16:35 +0900

    [add]DbContextを追加

---
+++ PasswordHash.App/Models/MyDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PasswordHash.App.Models {
+  public class MyDbContext : DbContext {
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+      : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+  }
+}
