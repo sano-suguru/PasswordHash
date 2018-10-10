ec04dfb764fed462f88bd4e0a2b409ef8bc4c7c5
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-10 23:46:01 +0900

    [add]パスワード検証のテスト

---
+++ PasswordHash.Lib.Test/PasswordServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace PasswordHash.Lib.Test {
+  public class PasswordServiceTest {
+    [Fact]
+    public void TestVerifyPassword() {
+
+      var rawPassword = "nossa1234";
+      var sut = new PasswordService();
+
+      var (hashed, salt) = sut.HashPassword(rawPassword);
+
+      Assert.True(sut.VerifyPassword(rawPassword, hashed, salt));
+    }
+  }
+}
