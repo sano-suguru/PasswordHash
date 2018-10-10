f27b146d61ca3e996bd8d5c1a5a83dc828219cac
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-10 23:24:59 +0900

    [update]Rename CheckPassword -> VerifyPassword

--- PasswordHash.Lib/IPasswordService.cs
+++ PasswordHash.Lib/IPasswordService.cs
@@ -1,6 +1,6 @@
 ﻿namespace PasswordHash.Lib {
   public interface IPasswordService {
-    bool CheckPassword(string rawPassword, string hashedPassword, string saltSring);
+    bool VerifyPassword(string rawPassword, string hashedPassword, string saltSring);
     (string hashedPassword, string salt) HashPassword(string rawPassword);
   }
 }
--- PasswordHash.Lib/PasswordService.cs
+++ PasswordHash.Lib/PasswordService.cs
@@ -2,7 +2,7 @@
 
 namespace PasswordHash.Lib {
   public class PasswordService : IPasswordService {
-    public bool CheckPassword(string rawPassword, string hashedPassword, string saltSring) => throw new NotImplementedException();
+    public bool VerifyPassword(string rawPassword, string hashedPassword, string saltSring) => throw new NotImplementedException();
     public (string hashedPassword, string salt) HashPassword(string rawPassword) => throw new NotImplementedException();
   }
 }
