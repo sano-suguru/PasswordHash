ce6c723ead6a67312c77c5287f16b2d321485298
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-12 00:12:04 +0900

    ユーザー登録と認証のインターフェースを作成

---
+++ PasswordHash.App/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordHash.App.Models;
+
+namespace PasswordHash.App.Services {
+  interface IUserService {
+    bool Register([FromBody] string username, [FromBody] string rawPassword);
+    bool Authenticate([FromBody] string username, [FromBody] string rawPassword);
+  }
+}
