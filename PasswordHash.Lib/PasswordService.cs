f10a7011d0d9539a34c11fb3168b4875eee1bafc
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-11 22:39:52 +0900

    [update]パスワードのハッシュ化関数を実装

--- PasswordHash.Lib/PasswordService.cs
+++ PasswordHash.Lib/PasswordService.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
 using System.Security.Cryptography;
 
 namespace PasswordHash.Lib {
   public class PasswordService : IPasswordService {
-    public (string hashedPassword, byte[] salt) HashPassword(string rawPassword) => throw new NotImplementedException();
+    public (string hashedPassword, byte[] salt) HashPassword(string rawPassword) {
+      byte[] salt = GetSalt();
+      string hashed = HashPassword(rawPassword, salt);
+      return (hashed, salt);
+    }
     public bool VerifyPassword(string rawPassword, string hashedPassword, byte[] salt) => throw new NotImplementedException();
+
+    string HashPassword(string rawPassword, byte[] salt) =>
+      Convert.ToBase64String(
+        KeyDerivation.Pbkdf2(
+        password: rawPassword,
+        salt: salt,
+        prf: KeyDerivationPrf.HMACSHA1,
+        iterationCount: 10000,
+        numBytesRequested: 256 / 8));
 
     byte[] GetSalt() {
       using (var gen = RandomNumberGenerator.Create()) {
