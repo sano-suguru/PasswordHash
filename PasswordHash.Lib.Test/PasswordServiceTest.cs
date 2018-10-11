a56d5b7dc9384b1dd91bd3b8146b4c9d9cc97984
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-11 22:51:55 +0900

    [update]コメントを追加

--- PasswordHash.Lib.Test/PasswordServiceTest.cs
+++ PasswordHash.Lib.Test/PasswordServiceTest.cs
@@ -4,12 +4,12 @@ namespace PasswordHash.Lib.Test {
   public class PasswordServiceTest {
     [Fact]
     public void TestVerifyPassword() {
-
+      // arange
       var rawPassword = "nossa1234";
-      var sut = new PasswordService();
-
+      var sut = new PasswordService();  // sut means System Under Test
+      // act
       var (hashed, salt) = sut.HashPassword(rawPassword);
-
+      // assert
       Assert.True(sut.VerifyPassword(hashed, rawPassword, salt));
     }
   }
