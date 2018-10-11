fb40a107e4597eab1a44b4a9d48a38ceddeab12a
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-12 00:15:13 +0900

    ユーザー登録処理を作成

--- PasswordHash.App/Services/UserService.cs
+++ PasswordHash.App/Services/UserService.cs
@@ -1,9 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PasswordHash.App.Models;
+using PasswordHash.Lib;
 using System;
+using System.Linq;
 
 namespace PasswordHash.App.Services {
   public class UserService : IUserService {
+    private readonly MyDbContext dbContext;
+    private readonly IPasswordService passwordService;
+
+    public UserService(MyDbContext dbContext, IPasswordService passwordService) {
+      this.dbContext = dbContext;
+      this.passwordService = passwordService;
+    }
+    public bool Register([FromBody] string username, [FromBody] string rawPassword) {
+      bool duplicated = dbContext.Users.Any(u => u.Name == username);
+      if (duplicated) {
+        return false;
+      }
+      (string hashed, byte[] salt) = passwordService.HashPassword(rawPassword);
+      dbContext.Users.Add(new User {
+        Name = username,
+        HashedPassword = hashed,
+        Salt = salt
+      });
+      dbContext.SaveChanges();
+      return true;
+    }
+
     public bool Authenticate([FromBody] string username, [FromBody] string rawPassword) => throw new NotImplementedException();
-    public bool Register([FromBody] string username, [FromBody] string rawPassword) => throw new NotImplementedException();
   }
 }
