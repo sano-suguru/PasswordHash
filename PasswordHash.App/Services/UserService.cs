7c6d7d1ca7b37b36d4fa13eecae4aac8b1aa9fab
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-12 00:13:50 +0900

    [add]UserServiceクラスを仮実装

---
+++ PasswordHash.App/Services/UserService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PasswordHash.App.Services {
+  public class UserService : IUserService {
+    public bool Authenticate([FromBody] string username, [FromBody] string rawPassword) => throw new NotImplementedException();
+    public bool Register([FromBody] string username, [FromBody] string rawPassword) => throw new NotImplementedException();
+  }
+}
