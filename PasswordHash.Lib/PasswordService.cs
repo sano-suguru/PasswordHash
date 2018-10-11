27602ab5b2be3c0ed239924cc27589b42157be57
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-11 22:35:18 +0900

    [add]ソルトを得る関数を実装

--- PasswordHash.Lib/PasswordService.cs
+++ PasswordHash.Lib/PasswordService.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace PasswordHash.Lib {
   public class PasswordService : IPasswordService {
     public (string hashedPassword, byte[] salt) HashPassword(string rawPassword) => throw new NotImplementedException();
     public bool VerifyPassword(string rawPassword, string hashedPassword, byte[] salt) => throw new NotImplementedException();
+
+    byte[] GetSalt() {
+      using (var gen = RandomNumberGenerator.Create()) {
+        var salt = new byte[128 / 8];
+        gen.GetBytes(salt);
+        return salt;
+      }
+    }
   }
 }
