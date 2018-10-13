444adfa3cf8396a391534f359ec0bc8f58f3c9a8
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-13 20:45:57 +0900

    [add]UsersContorllerを作成

---
+++ PasswordHash.App/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordHash.App.Services;
+
+namespace PasswordHash.App.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UsersController : ControllerBase {
+    private readonly IUserService userService;
+
+    public UsersController(IUserService userService) {
+      this.userService = userService;
+    }
+  }
+}
