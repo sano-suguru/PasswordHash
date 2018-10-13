1521581153e2220e5e2152ed386cc95058943e99
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-13 21:32:14 +0900

    [add]LoginControllerを作成

---
+++ PasswordHash.App/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordHash.App.Services;
+
+namespace PasswordHash.App.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class LoginController : ControllerBase {
+    private readonly IUserService userService;
+
+    public LoginController(IUserService userService) {
+      this.userService = userService;
+    }
+
+    public IActionResult Authenticate([FromBody] string username, [FromBody] string rawPassword) {
+      bool ok = userService.Authenticate(username, rawPassword);
+      if (ok) {
+        return Ok();
+      } else {
+        return Unauthorized();
+      }
+    }
+  }
+}
